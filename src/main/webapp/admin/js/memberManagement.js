7ed493a50912858e716eeb1e1579d81fc2888ab3
lgpc <leebduk@gmail.com>
2015-07-24 15:20:15 +0900

    회원추가 수정

--- src/main/webapp/admin/js/memberManagement.js
+++ src/main/webapp/admin/js/memberManagement.js
@@ -5,7 +5,10 @@ function memberModify(data) {
 		alert("오류가 발생했습니다.\n계속적으로 발생시 관리자께 해당 메시지를 캡쳐하여 보내주세요.\n오류 코드: " + data.resData[0].errorCd + "\n오류 메시지: " + data.resData[0].errorMsg);
 	}
 }
-
+function putAddUser(location){
+	location.html("<button href=\"#myModal\" data-toggle=\"modal\">회원 추가</button>");
+	location.css("padding-bottom: 10px");
+}
 function deleteMember(data) {
 	
 	if (data.result == "success") {
@@ -19,6 +22,7 @@ function deleteMember(data) {
 }
 function getMemberColumn(data) {
 	if (data.result == "success") {
+		
 		var rsltData = data.resData[0].attrList;
 		var memberInfoList = "";
 		memberInfoList += "<tr>";
@@ -41,6 +45,7 @@ function getMemberColumn(data) {
 
 function getMemberList(data) {
 	if (data.result == "success") {
+		putAddUser($("#addUser"))
 		var memberInfoList = "";
 		for (i = 0; i < data.resData[0].userList.length; i++) {
 			memberInfoList += "<tr id=\"" + data.resData[0].userList[i].userNo + "\"> \n";
