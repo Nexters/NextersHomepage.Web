2be28884fb89fc99030d0be18a36521cfe6c64e1
lgpc <leebduk@gmail.com>
2015-07-26 18:57:04 +0900

    asd

--- src/main/webapp/admin/js/attendenceManagement.js
+++ src/main/webapp/admin/js/attendenceManagement.js
@@ -135,7 +135,15 @@ function userActiviyList(data) {
 		alert("오류가 발생했습니다.\n계속적으로 발생시 관리자께 해당 메시지를 캡쳐하여 보내주세요.\n오류 코드: " + data.resData[0].errorCd + "\n오류 메시지: " + data.resData[0].errorMsg);
 	}
 }
+function booksValueList(){
+	if (data.result == "success") {
 
+		alert(data.resData[0].toString);
+		$("#activityList").html(memberNameList)
+	} else {
+		alert("오류가 발생했습니다.\n계속적으로 발생시 관리자께 해당 메시지를 캡쳐하여 보내주세요.\n오류 코드: " + data.resData[0].errorCd + "\n오류 메시지: " + data.resData[0].errorMsg);
+	}
+}
 $(document).ready(function() {
 
 	$(".table").css("width", '0px');
