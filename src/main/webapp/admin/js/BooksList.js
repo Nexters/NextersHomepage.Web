3c4c38da35b985e9128a6a599b2aaa5a2a887c87
Rick Lim <wow5468@naver.com>
2015-07-29 04:07:45 +0900

    Tab 전환 수정

--- src/main/webapp/admin/js/BooksList.js
+++ src/main/webapp/admin/js/BooksList.js
@@ -32,6 +32,8 @@ function BooksDetail(bookNo, itsme) {
 	$("#BookListDiv").fadeOut(function(){
 		$("#BookDetailDiv").fadeIn();
 		$("#booksName").html($(itsme).html());
+		$("#dateList").html("");
+		$("#activityList").html("");
 		$(".custom-table").css("width", '0px');
 		requestJsonData("api/admin/getDateList.do", {
 			bookNm : thisName
