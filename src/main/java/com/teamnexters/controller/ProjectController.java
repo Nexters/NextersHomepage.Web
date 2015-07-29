030c9db4644fc684bc0a38c64539fd529d1c0987
KimSeongIl <ksi4687@nate.com>
2015-07-29 15:09:17 +0900

    파일 업로드 추가

---
+++ src/main/java/com/teamnexters/controller/ProjectController.java
@@ -0,0 +1,70 @@
+package com.teamnexters.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.teamnexters.dao.ProjectDAO;
+import com.teamnexters.dto.ProjectDTO;
+import com.teamnexters.util.JsonUtil;
+
+@Controller
+public class ProjectController {
+
+	@Autowired
+	ProjectDAO projectDao;
+
+	@RequestMapping("projectAdd.do")
+	@Autowired
+	public @ResponseBody Map<String,Object> projectAdd(ProjectDTO fileDto) {
+
+		MultipartFile uploadFile = fileDto.getUploadFile();
+
+
+		if (uploadFile != null) {
+
+			String fileName = uploadFile.getOriginalFilename();
+
+			Date date=new Date();
+			int year=date.getYear()-100;
+			int month=date.getMonth()+1;
+			int day=date.getDate();
+			int hour=date.getHours();
+			int minute=date.getMinutes();
+			int second=date.getSeconds();
+			String time=year+""+month+""+day+""+hour+""+minute+""+second;
+			int comma=fileName.lastIndexOf(".");
+			String pre=fileName.substring(0,comma);
+			String end=fileName.substring(comma+1,fileName.length());
+			fileName=pre+"_"+time+"."+end;
+
+
+
+
+			try {
+
+				File file = new File("C:/images/" + fileName);
+
+
+
+				uploadFile.transferTo(file);
+
+				fileDto.setProjectImg(fileName);
+				System.out.println(fileDto);
+				projectDao.insertProject(fileDto);
+			} catch (IOException e) {
+				e.printStackTrace();
+			} // try - catch
+		} // if
+
+		return  JsonUtil.putSuccessJsonContainer(null);
+
+	}
+}
--- src/main/java/com/teamnexters/dao/ProjectDAO.java
+++ src/main/java/com/teamnexters/dao/ProjectDAO.java
@@ -3,11 +3,17 @@ package com.teamnexters.dao;
 import org.apache.ibatis.logging.Log;
 import org.apache.ibatis.logging.LogFactory;
 import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProjectDAO {
 	 protected Log log = LogFactory.getLog(LoginDAO.class);
 
-	    private SqlSession SqlSession;
+	 	@Autowired
+	    private SqlSession sqlSession;
+	    
+	    public void insertProject(Object param){
+	    	sqlSession.insert("insertProject",param);
+	    }
 }
