b3834f669882a5ab149f28c1fc36641e2422c6e6
Rick Lim <wow5468@naver.com>
2015-08-26 03:17:41 +0900

    준범이형 메인 Homepage 수정 적용

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,54 +1,64 @@
 var app = angular
-  .module("app",[
-    'Activites.productController',
-    'Activites.memberController',
-    'ui.router',
-    'productFactory',
-    'memberFactory',
-    'ui.bootstrap',
-    'carousel'
-  ]).config(['$urlRouterProvider','$stateProvider', function ($urlRouterProvider,$stateProvider) {
-      $stateProvider
-        //About
-        .state('About',{
-          url:'/',
-          templateUrl: "templates/About/About.html"
-        })
-        .state('Test',{
-          url:'/test',
-          templateUrl: "templates/test.html"
-        })
-        //----------Activities start----------------
-        .state('Activities',{
-          url:'/Activities',
-          templateUrl: "templates/Activities/Activities.html",
+.module("app",[
+  'Activites.productController',
+  'Activites.memberController',
+  'ui.router',
+  'productFactory',
+  'memberFactory',
+  'ui.bootstrap',
+  'carousel'
+]).config(['$urlRouterProvider','$stateProvider', function ($urlRouterProvider,$stateProvider) {
+  $stateProvider
+  //About
+  .state('About',{
+    url:'/',
+    templateUrl: "templates/About/About.html"
+  })
+  .state('Test',{
+    url:'/test',
+    templateUrl: "templates/test.html"
+  })
+  //----------Activities start----------------
+  .state('Activities',{
+    url:'/Activities',
+    templateUrl: "templates/Activities/Activities.html",
 
-        })
-        .state('Activities.member',{
-          url:'/member',
-          templateUrl: "templates/Activities/Activities.member.html",
-          controller: 'Activites.memberController'
-        })
-        .state('Activities.product',{
-          url:'/product',
-          templateUrl: "templates/Activities/Activities.product.html",
-          controller: 'Activites.productController'
-        })
-        //Activities-product end
+  })
+  .state('Activities.member',{
+    url:'/member',
+    templateUrl: "templates/Activities/Activities.member.html",
+    controller: 'Activites.memberController'
+  })
+  .state('Activities.product',{
+    url:'/product',
+    templateUrl: "templates/Activities/Activities.product.html",
+    controller: 'Activites.productController'
+  })
+  //Activities-product end
 
-        //Contact
-        .state('Contact',{
-          url:'/contact',
-          templateUrl: "templates/Contact/Contact.html",
-        })
-        .state('Contact.sendTo',{
-          url:'/sendTo',
-          templateUrl: "templates/Contact/Contact.sendTo.html",
-        })
-        //Contact end
+  //Contact
+  .state('Contact',{
+    url:'/contact',
+    templateUrl: "templates/Contact/Contact.html",
+  })
+  .state('Contact.sendTo',{
+    url:'/sendTo',
+    templateUrl: "templates/Contact/Contact.sendTo.html",
+  })
+  //Contact end
 
-        $urlRouterProvider.otherwise('/');
+  $urlRouterProvider.otherwise('/');
 }])
 .value('environment',{
   domain: "/"
+})
+.controller('navControler',function ($scope,$location,$anchorScroll) {
+  $scope.gotoTop = function (){
+    // set the location.hash to the id of
+    // the element you wish to scroll to.
+    $location.hash('top');
+
+    // call $anchorScroll()
+    $anchorScroll();
+  };
 })
